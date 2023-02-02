575c03aa0fe70fe774112f434bcdc6eab4f7e03e
JasonBeach11 <119906700+JasonBeach11@users.noreply.github.com>
2023-02-01 17:29:25 -0700

    Create Triangle.java

---
+++ Chapter 13/Exercise13-7/Triangle.java
@@ -0,0 +1,75 @@
+
+import java.lang.Math;
+public class Triangle extends GeometricObject implements Colorable{
+	
+	private double side1 = 1.0;
+	private double side2 = 1.0;
+	private double side3 = 1.0;
+
+	
+	/** No-arg default constructor */
+	public Triangle() {
+	}
+
+	
+	/** A constructor that creates a triangle with the specified side1, side2, and side3 */
+	public Triangle(double side1, double side2, double side3) {
+		this.side1 = side1;
+		this.side2 = side2;
+		this.side3 = side3;
+	}
+
+	
+	/** Accessor method for side1 */
+	public double getSide1() {
+		return side1;
+	}
+	
+	
+	/** Accessor method for side2 */
+	public double getSide2() {
+		return side2;
+	}
+	
+	
+	/** Accessor method for side3 */
+	public double getSide3() {
+		return side3;
+	}
+	
+	
+	/** Return the area of this triangle */
+	public double getArea() {
+		
+		// calculate triangle area 
+		double s = (side1 + side2 + side3) / 2;
+		double area = Math.sqrt(s * (s - side1)*(s - side2)*(s - side3));
+		
+		return area;
+	}
+	
+	
+	/** Return the perimeter of this triangle */
+	public double getPerimeter() {
+		
+		// calculate perimeter
+		double perimeter = side1 + side2 + side3;
+		
+		return perimeter;
+	}
+	
+	
+	/** Return string description for the triangle */
+	public String toString() {
+		return "Triangle: side1 = " + side1 + " side2 = " + side2 + " side3 = " + side3; 
+	}
+	
+	
+	/** Describe how to color a triangle */
+	@Override
+	public String howToColor() {
+		return "Color all three sides.";
+	}
+	
+	
+} // End Triangle class
